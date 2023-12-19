33b2cdcf902e30adc1b8de36297ac6db1be5fa1a
liudf <liudf@autoxing.com>
2023-12-19 14:52:22 +0800

    use UInt8MultiArray

--- src/main.cpp
+++ src/main.cpp
@@ -3,51 +3,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
-
 #include "com_stream.h"
-#include "light_flow/OpticalFlowPack.h"
-
-struct __attribute__((packed)) BottomSensorPacket
-{
-    char header[2];
-    uint16_t length;
-    uint16_t crc16;
-    uint8_t payload[0];
-};
-
-struct __attribute__((packed)) OpticalFlowPack
-{
-    BottomSensorPacket header;
-    int16_t flow_x_integral;
-    int16_t flow_y_integral;
-    uint16_t integration_timespan; // unit: us
-    uint16_t ground_distance;
-    uint8_t valid; // 0x00 invalid; 0xf5 valid
-    uint8_t version;
-};
-
-uint16_t calculateCRC16(const uint8_t *p, int len)
-{
-    uint16_t crc = 0xffff;            /// init value
-    const uint16_t CRC_MASK = 0xA001; /// high and low bit flipping of '0x8005'
-    int i, j;
-    for (j = 0; j < len; j++)
-    {
-        crc ^= p[j];
-        for (i = 0; i < 8; i++)
-        {
-            if ((crc & 0x0001) > 0)
-            {
-                crc = (crc >> 1) ^ CRC_MASK;
-            }
-            else
-            {
-                crc >>= 1;
-            }
-        }
-    }
-    return crc;
-}
 
 int main(int argc, char **argv)
 {
@@ -75,14 +31,13 @@ int main(int argc, char **argv)
 
     const int bufferSize = 1024;
     uint8_t buffer[bufferSize];
-    int bytesRead;
+    int bytesRead = 0;
     ros::Rate rate(120);
 
     std_msgs::UInt8MultiArray msg;
     msg.data.resize(bufferSize);
     while (ros::ok())
     {
-        msg.data.clear();
         ros::spinOnce();
 
         if (reader.hasHandleError())
@@ -91,7 +46,7 @@ int main(int argc, char **argv)
             break;
         }
 
-        bytesRead = reader.read(buffer, bufferSize);
+        bytesRead = reader.read(msg.data.data(), bufferSize);
         if (bytesRead == -1 && (errno != EINTR && errno != EAGAIN))
         {
             ROS_ERROR("read data error, [ret = %d, errno = %d] will exit...", bytesRead, errno);
@@ -99,11 +54,8 @@ int main(int argc, char **argv)
             break;
         }
 
-        ROS_INFO("bytesRead is %d", bytesRead);
-        for (int i = 0; i < bytesRead; i++)
-        {
-            msg.data.push_back(buffer[i]);
-        }
+        msg.data.resize(bytesRead);
+        ROS_INFO("bytesRead is %d, data size is %zu", bytesRead, msg.data.size());
 
         pub.publish(msg);
         rate.sleep();
